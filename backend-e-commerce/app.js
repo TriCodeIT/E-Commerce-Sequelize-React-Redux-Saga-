87791d2159971d883a2a08bcc4524b03f12d996f
TriCodeIT <bayu165kusuma@gmail.com>
2020-09-08 15:32:34 +0700

    add routes product

--- backend-e-commerce/app.js
+++ backend-e-commerce/app.js
@@ -3,6 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var cors = require('cors');
+const fileUpload = require('express-fileupload');
+
 var productsRouter = require('./routes/products');
 var usersRouter = require('./routes/users');
 
@@ -13,6 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+app.use(fileUpload({ limits: { fileSize: 4 * 1024 * 1024 } }))
 
 app.use('/api/product', productsRouter);
 app.use('/users', usersRouter);
--- backend-e-commerce/routes/products.js
+++ backend-e-commerce/routes/products.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 var models = require('../models/index');
 
+const path = require('path');
+const product = require('../models/product');
+const server_URL = "http://localhost:3000";
+
 /* GET products listing. */
 router.get('/', function (req, res, next) {
   models.product.findAll({
@@ -11,6 +15,31 @@ router.get('/', function (req, res, next) {
   }).catch((err) => {
     res.json(err)
   })
+})
+
+// POST Add Products
+router.post('/', (req, res) => {
+ 
+  let { title, rate, description, price, brand, detailProduct, category, image, color, capacities, stock, size } = req.body;
+  
+      models.product.create({
+        title,
+        rate,
+        description,
+        price,
+        brand,
+        detail_product: detailProduct,
+        category,
+        image,
+        color: color.split(','),
+        stock,
+        size: size,
+        capacities: capacities.split(',')
+      }).then(function (product) {
+        res.json(product);
+      }).catch((err) => {
+        res.status(500).json(err)
+      })
 })
 
 
